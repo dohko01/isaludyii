e7ef6f6efff66f3db54ed57f1b4da6cb4bc850ba
dohko01 <libranodohko1@gmail.com>
2013-10-11 15:04:30 -0500

    Cambio de tema a Classic

--- isalud/protected/config/main.php
+++ isalud/protected/config/main.php
@@ -8,7 +8,7 @@
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'iSalud',
-	'theme'=>'abound',
+	'theme'=>'classic',
 	'language'=>'es',
 
 	// preloading 'log' component
