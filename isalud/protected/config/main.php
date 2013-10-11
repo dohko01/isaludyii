f07be628190d0d290888f7a6b1ee7d7d1f78b55d
p4scu41 <pereznango@gmail.com>
2013-10-11 15:02:05 -0500

    Establece el lenguaje a español

--- isalud/protected/config/main.php
+++ isalud/protected/config/main.php
@@ -9,6 +9,7 @@ return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'iSalud',
 	'theme'=>'abound',
+	'language'=>'es',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
