2b13b929c796d0abd4ba388d8e0464c12384bb84
dohko01 <libranodohko1@gmail.com>
2013-10-17 13:17:22 -0500

    Modificaciones a las vistas de Direccion y Subdireccion

--- isalud/protected/views/direccion/_view.php
+++ isalud/protected/views/direccion/_view.php
@@ -4,11 +4,11 @@
 ?>
 
 <div class="view">
-
+<?php /*
 	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
 	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
 	<br />
-
+*/ ?>
 	<b><?php echo CHtml::encode($data->getAttributeLabel('id_cat_institucion')); ?>:</b>
 	<?php echo CHtml::encode(($data->idCatInstitucion!=null) ? $data->idCatInstitucion->nombre : null); ?>
 	<br />
